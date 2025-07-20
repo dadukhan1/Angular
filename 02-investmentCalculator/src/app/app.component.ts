b72fb8182c2667aa05048032b65501267d2daba2
Dadu Khan <photoshopt65@gmail.com>
2025-07-20 14:34:37 +0500

    changing to service

--- 02-investmentCalculator/src/app/app.component.ts
+++ 02-investmentCalculator/src/app/app.component.ts
@@ -10,40 +10,4 @@ import { InvestmentResultsComponent } from './investment-results/investment-resu
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  resultData = signal<
-    | {
-        year: number;
-        interest: number;
-        valueEndOfYear: number;
-        annualInvestment: number;
-        totalInterest: number;
-        totalAmountInvested: number;
-      }[]
-    | undefined
-  >(undefined);
-
-  onCalculateInvestmentResults(data: InvestmentInput) {
-    const annualData = [];
-    const { initialInvestment, duration, annualInvestment, expectedReturn } =
-      data;
-    let investmentValue = initialInvestment;
-
-    for (let i = 0; i < duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-      investmentValue += interestEarnedInYear + annualInvestment;
-      const totalInterest =
-        investmentValue - annualInvestment * year - initialInvestment;
-      annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: annualInvestment,
-        totalInterest: totalInterest,
-        totalAmountInvested: initialInvestment + annualInvestment * year,
-      });
-    }
-    this.resultData.set(annualData);
-  }
-}
+export class AppComponent {}
