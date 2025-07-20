158e71deb212e19304bc05673be3380bc494609e
Dadu Khan <photoshopt65@gmail.com>
2025-07-20 11:46:23 +0500

    investmentCalculatorAppComopleted

---
+++ 02-investmentCalculator/src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from './user-input/user-input.component';
+import { InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  resultData?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
+
+  onCalculateInvestmentResults(data: InvestmentInput) {
+    const annualData = [];
+    const { initialInvestment, duration, annualInvestment, expectedReturn } =
+      data;
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+    this.resultData = annualData;
+  }
+}
