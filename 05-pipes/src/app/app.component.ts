90af89e0e27060eb826a661e046387e6581c83d3
Dadu Khan <photoshopt65@gmail.com>
2025-07-26 18:40:28 +0500

    completed

--- 05-pipes/src/app/app.component.ts
+++ 05-pipes/src/app/app.component.ts
@@ -1,11 +1,14 @@
+import { DatePipe, DecimalPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TemperaturePipe } from './temp.pipe';
+import { SortPipe } from './sort.pipe';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [DatePipe, TemperaturePipe, SortPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   currentDate = new Date();
@@ -20,7 +23,15 @@ export class AppComponent {
     25, 37, 19, -4, 28, 21, 19, 28, 33, 31, 9, 11, 5, -12, -5,
   ];
 
+  constructor() {
+    this.historicTemperatures.sort((a, b) => {
+      return a > b ? 1 : -1;
+    });
+  }
+
   onReset(index: number) {
-    this.historicTemperatures[index] = 18;
+    const newTemp = [...this.historicTemperatures];
+    newTemp[index] = 18;
+    this.historicTemperatures = newTemp;
   }
 }
---
+++ 05-pipes/src/app/temp.pipe.ts
@@ -0,0 +1,38 @@
+import { input, output, Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'temp',
+})
+export class TemperaturePipe implements PipeTransform {
+  transform(
+    value: number | string,
+    inputType: 'fah' | 'cel',
+    outputType?: 'fah' | 'cel'
+  ) {
+    let val: number;
+
+    if (typeof value === 'string') {
+      val = parseFloat(value);
+    } else {
+      val = value;
+    }
+
+    let outputTemp;
+    if (inputType === 'cel' && outputType === 'fah') {
+      outputTemp = val * (9 / 5) + 32;
+    } else if (inputType === 'fah' && outputType === 'cel') {
+      outputTemp = (val - 32) * (5 / 9);
+    } else {
+      outputTemp = val;
+    }
+
+    let symbol: '°F' | '°C';
+    if (!outputType) {
+      symbol = inputType === 'cel' ? '°C' : '°F';
+    } else {
+      symbol = outputType === 'cel' ? '°C' : '°F';
+    }
+
+    return `${outputTemp.toFixed(2)} ${symbol}`;
+  }
+}
